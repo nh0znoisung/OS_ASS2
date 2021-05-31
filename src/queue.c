c84e044397dd7bb5ddb64fa4005157e6008b41a0
= <55056655+nh0znoisung@users.noreply.github.com>
2021-05-31 12:51:15 +0700

    Update enqueue

--- src/queue.c
+++ src/queue.c
@@ -8,8 +8,11 @@ int empty(struct queue_t * q) {
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
 	/* TODO: put a new process to queue [q] */
-	q->proc[q->size] = proc;
-	q->size++;	
+	if(q->size < MAX_QUEUE_SIZE){
+		q->proc[q->size] = proc;
+		q->size++;	
+	}
+	
 }
 
 struct pcb_t * dequeue(struct queue_t * q) {
